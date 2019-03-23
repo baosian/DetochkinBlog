bbfda25059945437dd35dea02c4c436d33c3e9c5
baosian <A7500543@gmail.com>
2019-03-24 01:18:56 +0300

    add config files

--- src/main/java/com/baosian/config/ServletInitializer.java
+++ src/main/java/com/baosian/config/ServletInitializer.java
@@ -1,4 +1,22 @@
 package com.baosian.config;
 
-public class ServletInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                DataConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
--- src/main/java/com/baosian/entity/User.java
+++ src/main/java/com/baosian/entity/User.java
@@ -1,17 +1,34 @@
 package com.baosian.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Data
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id", nullable = false)
     private Integer id;
+
+    @Column(name = "first_name")
     private String first_name;
+
+    @Column(name = "last_name")
     private String last_name;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "created_at")
     private Date created_at;
 }
