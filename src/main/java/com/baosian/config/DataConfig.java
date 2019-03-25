268c702338f3ee9a031cea6ab0b1a2449039c794
baosian <A7500543@gmail.com>
2019-03-26 01:36:49 +0300

    fix spring-core version bug

--- src/main/java/com/baosian/config/DataConfig.java
+++ src/main/java/com/baosian/config/DataConfig.java
@@ -54,7 +54,7 @@ public class DataConfig {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(dataSource());
         entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-        //entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGES_TO_SCAN));
+        entityManagerFactoryBean.setPackagesToScan(env.getRequiredProperty(PROP_ENTITYMANAGER_PACKAGES_TO_SCAN));
         entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
 
         return entityManagerFactoryBean;
--- src/main/java/com/baosian/controllers/users/UserCreationController.java
+++ src/main/java/com/baosian/controllers/users/UserCreationController.java
@@ -1,7 +1,12 @@
 package com.baosian.controllers.users;
 
+import com.baosian.services.UserService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserCreationController {
+
+    private UserService userService;
+
+
 }
